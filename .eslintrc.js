e9847d15837900abe6d8061668830843b9889fd5
pb <good_luck_pb@163.com>
2019-11-03 15:42:19 +0800

    '完成了登录功能’

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   root: true,
   env: {
@@ -9,7 +10,8 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
