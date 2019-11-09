7eea314bfef96177753bb2a6ba7e27f146e8eb4f
你又犹豫了 <good_luck_pb@163.com>
2019-11-09 20:51:41 +0800

    '完成了分类参数的开发'

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
@@ -9,9 +9,9 @@ import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const router = new VueRouter({
+const router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
@@ -22,8 +22,8 @@ const router = new VueRouter({
       children: [
         { path: '/welcome', component: Welcome },
         { path: '/users', component: Users },
-        { path: '/Rights', component: Rights },
-        { path: '/Roles', component: Roles },
+        { path: '/rights', component: Rights },
+        { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
         { path: '/params', component: Params }
       ]
@@ -37,7 +37,9 @@ router.beforeEach((to, from, next) => {
   // from 代表从哪个路径跳转而来
   // next 是一个函数，表示放行
   //     next()  放行    next('/login')  强制跳转
+
   if (to.path === '/login') return next()
+  // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
   if (!tokenStr) return next('/login')
   next()
